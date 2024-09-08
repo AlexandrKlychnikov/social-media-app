87ea5c84e90920c1f16c1d86da4ad482f61d5841
Klychnikov Alexandr <klychnikov75@mail.ru>
2024-09-08 16:06:44 +0300

    feat: create sign-in form

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -68,7 +68,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     const cookieFallback = localStorage.getItem('cookieFallback');
-    if (cookieFallback === '[]') {
+    if (cookieFallback === '[]' || cookieFallback === null) {
       navigate('/sign-in');
     }
 
