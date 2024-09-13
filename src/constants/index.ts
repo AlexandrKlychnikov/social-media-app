ee9ecf9182c2640d25ddce20b479272792ce0fdc
Klychnikov Alexandr <klychnikov75@mail.ru>
2024-09-13 22:07:23 +0300

    feat: create bottombar witn links for navigation on mobile size

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,11 +17,34 @@ export const sidebarLinks = [
   {
     imgURL: '/assets/icons/bookmark.svg',
     route: '/saved',
-    label: 'Сохраненные',
+    label: 'Закладки',
   },
   {
     imgURL: '/assets/icons/post-add.svg',
     route: '/create-post',
-    label: 'Создать пост',
+    label: 'Создать',
+  },
+];
+
+export const bottombarLinks = [
+  {
+    imgURL: '/assets/icons/home.svg',
+    route: '/',
+    label: 'Главная',
+  },
+  {
+    imgURL: '/assets/icons/explore.svg',
+    route: '/explore',
+    label: 'Поиск',
+  },
+  {
+    imgURL: '/assets/icons/bookmark.svg',
+    route: '/saved',
+    label: 'Закладки',
+  },
+  {
+    imgURL: '/assets/icons/post-add.svg',
+    route: '/create-post',
+    label: 'Создать',
   },
 ];
